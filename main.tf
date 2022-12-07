fb60a37995c6ef5e8a7ded1bbdc66cbe0a141e81
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2022-12-07 13:25:29 +0000

    app request 001

--- main.tf
+++ main.tf
@@ -5,6 +5,8 @@ module "workspace-vending" {
   source  = "app.terraform.io/djs-tfcb/workspace-vending/tfe"
   version = "3.0.8"
 
-  app_ids = []
+  app_ids = [
+    "001"
+  ]
   app_envs = [ "development", "uat", "production" ]
 }
