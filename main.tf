552df2b00652dc1030676273f708d057defa1f1c
mikefons <fonseca.michael@gmail.com>
2022-12-07 13:17:05 -0500

    first test

--- main.tf
+++ main.tf
@@ -5,9 +5,9 @@ module "workspace-vending" {
   source  = "app.terraform.io/djs-tfcb/workspace-vending/tfe"
   version = "4.0.0"
 
-  tf_org = "djs-tfcb"
-  gh_org = "djschnei21"
-  oauth_client_name = "github-test"
+  tf_org = "mfonseca-org"
+  gh_org = "mikefons"
+  oauth_client_name = "Github.com"
 
   app_ids = [
     "001",
