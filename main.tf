85b7544aa352f595ea8861c1075b4cedf36915d1
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2022-12-07 12:35:42 +0000

    toset into module

--- main.tf
+++ main.tf
@@ -5,8 +5,8 @@ module "workspace-vending" {
   source  = "app.terraform.io/djs-tfcb/workspace-vending/tfe"
   version = "1.0.0"
 
-  app_ids = toset( [
+  app_ids = [
     "001"
-  ] )
+  ]
   app_envs = [ "development", "uat", "production" ]
 }
