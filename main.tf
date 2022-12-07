2e9e3442ce1ca1c333b3d20556d6003d34b49d9b
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2022-12-07 13:23:50 +0000

    remove all apps

--- main.tf
+++ main.tf
@@ -5,10 +5,6 @@ module "workspace-vending" {
   source  = "app.terraform.io/djs-tfcb/workspace-vending/tfe"
   version = "3.0.8"
 
-  app_ids = [
-    "001",
-    "002",
-    "003"
-  ]
+  app_ids = []
   app_envs = [ "development", "uat", "production" ]
 }
