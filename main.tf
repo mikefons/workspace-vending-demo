c82cf3f368160b4a933dbe9edfa87c029061e2c8
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2022-12-07 07:26:48 -0500

    Initial commit

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+    doormat = {
+      source  = "doormat.hashicorp.services/hashicorp-security/doormat"
+      version = "~> 0.0.2"
+    }
+  }
+}
+
+provider "aws" {
+  access_key = data.doormat_aws_credentials.creds.access_key
+  secret_key = data.doormat_aws_credentials.creds.secret_key
+  token      = data.doormat_aws_credentials.creds.token
+  region     = ""
+}
