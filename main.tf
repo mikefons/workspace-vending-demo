b0ee22dbd8e4ca03a9dcc98da2b4ba6728f3a361
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2022-12-07 15:02:12 +0000

    module bump and additional variables

--- main.tf
+++ main.tf
@@ -3,7 +3,11 @@ terraform {
 
 module "workspace-vending" {
   source  = "app.terraform.io/djs-tfcb/workspace-vending/tfe"
-  version = "3.0.16"
+  version = "4.0.0"
+
+  tf_org = "djs-tfcb"
+  gh_org = "djschnei21"
+  oauth_client_name = "github-test"
 
   app_ids = [
     "001",
